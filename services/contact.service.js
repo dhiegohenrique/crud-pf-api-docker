06c49fdbdf565c20a3d3704c054d081d1e698c43
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-06 14:20:13 -0300

    add validate contact

--- services/contact.service.js
+++ services/contact.service.js
@@ -1,5 +1,6 @@
 const Contact = require('../models/Contact')
 const baseService = require('./base.service')
+const brazilianUtils = require('@brazilian-utils/brazilian-utils')
 
 const update = (contact) => {
   return baseService.update(Contact, contact)
@@ -17,9 +18,54 @@ const deleteItem = (_id) => {
   return baseService.deleteItem(Contact, _id)
 }
 
+const validate = (contact) => {
+  return new Promise((resolve) => {
+    if (!contact || !contact.length) {
+      return resolve({
+        isValid: false,
+        message: 'Informe o celular.'
+      })
+    }
+
+    if (!Array.isArray) {
+      contact = [contact]
+    }
+
+    const errors = []
+    contact.forEach(async (currentContact, index) => {
+      currentContact = currentContact.replace(/\D/g, '')
+      const error = {
+        contactIndex: index
+      }
+
+      if (!currentContact) {
+        error.message = 'Informe o celular'
+      } else if (!brazilianUtils.isValidPhone(currentContact)) {
+        error.message = 'Informe um celular válido.'
+      }
+
+      if (Object.keys(error).length > 1) {
+        errors.push(error)
+      }
+
+      if (index === (contact.length - 1)) {
+        resolve({
+          isValid: !errors.length,
+          message: {
+            contact: {
+              errors
+            }
+          }
+        })
+      }
+    })
+  })
+}
+
 module.exports = {
   update,
   get,
   insert,
-  deleteItem
+  deleteItem,
+  validate
 }
