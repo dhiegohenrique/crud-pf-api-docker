d78361f27faabb3df3c4c6d7c6ab7b793f830e38
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-06 13:36:48 -0300

    remove uf model

--- models/Uf.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const ufSchema = new mongoose.Schema({
-  uf: {
-    type: String,
-    required: true
-  }
-})
-
-module.exports = mongoose.model('Uf', ufSchema)
