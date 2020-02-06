d0a0f02bc9ed39265b0b0a247bb02dfebc4093dd
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-06 09:41:42 -0300

    add routes

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const homeController = require('../controllers/homeController')
+const personController = require('../controllers/personController')
+
+const routes = [
+  {
+    method: 'GET',
+    url: '/',
+    handler: homeController.home
+  },
+  {
+    method: 'POST',
+    url: '/person',
+    handler: personController.insert
+  },
+  {
+    method: 'GET',
+    url: '/person',
+    handler: personController.get
+  },
+  {
+    method: 'PUT',
+    url: '/person',
+    handler: personController.update
+  },
+  {
+    method: 'DELETE',
+    url: '/person/:id',
+    handler: personController.delete
+  }
+]
+
+module.exports = routes
