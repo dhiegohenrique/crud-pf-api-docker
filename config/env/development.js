f5f6b0594610177933cfb5bd7f8cc2240674e0d7
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-07 16:31:05 -0300

    remove url

--- config/env/development.js
+++ config/env/development.js
@@ -2,5 +2,5 @@ module.exports = {
   env: 'development',
   db: 'mongodb://localhost/crud-pf-api',
   port: 3000,
-  url_client: ['http://localhost:8080', 'http://localhost:3001']
+  url_client: ['http://localhost:8080']
 }
--- config/env/test.js
+++ config/env/test.js
@@ -2,5 +2,5 @@ module.exports = {
   env: 'test',
   db: 'mongodb://127.0.0.1:27017/crud-pf-api-test',
   port: 3002,
-  url_client: ['http://localhost:8080', 'http://localhost:3001']
+  url_client: ['http://localhost:8080']
 }
