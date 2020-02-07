6aabebae7a23a5ee41a4d7f94e67145a9482c0b4
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-07 10:54:23 -0300

    add populate states

--- db/db.js
+++ db/db.js
@@ -1,23 +1,77 @@
 const mongoose = require('mongoose')
 const config = require('../config/config')()
 const mongooseRegisterEvents = require('./mongooseRegisterEvents')
+const State = require('../models/State')
 
-const connect = async () => {
-  try {
-    mongooseRegisterEvents(mongoose)
-    await mongoose.connect(config.db, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
-    console.log('MongoDB connected…')
-  } catch (err) {
-    console.log('Mongo error: ' + err)
-    process.exit(1)
-  }
+const connect = () => {
+  return new Promise((resolve) => {
+    try {
+      mongooseRegisterEvents(mongoose)
+      mongoose.connect(config.db, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      })
+        .then(async () => {
+          console.log('MongoDB connected…')
+          await populateStates()
+          resolve()
+        })
+    } catch (err) {
+      console.log('Mongo error: ' + err)
+      process.exit(1)
+    }
+  })
 }
 
-const dropDatabase = async () => {
-  await mongoose.connection.dropDatabase()
+const populateStates = () => {
+  return new Promise((resolve) => {
+    const states = [
+      { uf: 'AC' },
+      { uf: 'AL' },
+      { uf: 'AP' },
+      { uf: 'AM' },
+      { uf: 'BA' },
+      { uf: 'CE' },
+      { uf: 'DF' },
+      { uf: 'ES' },
+      { uf: 'GO' },
+      { uf: 'MA' },
+      { uf: 'MT' },
+      { uf: 'MS' },
+      { uf: 'MG' },
+      { uf: 'PA' },
+      { uf: 'PB' },
+      { uf: 'PR' },
+      { uf: 'PE' },
+      { uf: 'PI' },
+      { uf: 'RJ' },
+      { uf: 'RN' },
+      { uf: 'RS' },
+      { uf: 'RO' },
+      { uf: 'RR' },
+      { uf: 'SC' },
+      { uf: 'SP' },
+      { uf: 'SE' },
+      { uf: 'TO' }
+    ]
+
+    states.forEach(async (uf, index) => {
+      let state = await State.findOne({ 'uf': { $regex: new RegExp(uf, 'i') } }).select('_id')
+      if (!state) {
+        state = new State(uf)
+        await state.save()
+        console.log(`Populate state '${uf.uf}'...`)
+      }
+
+      if (index === (states.length - 1)) {
+        resolve()
+      }
+    })
+  })
+}
+
+const dropDatabase = () => {
+  return mongoose.connection.dropDatabase()
 }
 
 module.exports = {
