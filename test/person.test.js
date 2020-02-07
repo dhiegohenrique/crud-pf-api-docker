b6fa719ca551fe80ed6eba813e4f539c8a51b255
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-07 10:56:07 -0300

    add should add new person

--- test/person.test.js
+++ test/person.test.js
@@ -1,13 +1,49 @@
 const expect = require('chai').expect
 const HttpStatus = require('http-status-codes')
+const cpf = require('@fnando/cpf/dist/node')
+const utils = require('./utils')
 
 describe('Person', () => {
   beforeEach(() => {
   })
 
-  describe('Should add new person', () => {
-    it('Teste de exemplo', () => {
-      expect(2).to.be.equal(2)
-    })
+  it('Should add new person', async () => {
+    const time = new Date().getTime()
+
+    const person = {
+      name: `Nome ${time}`,
+      'cpf': cpf.generate(),
+      email: `email${time}@email.com`,
+      birthDate: new Date(),
+      address: [
+        {
+          street: `Rua ${time}`,
+          cep: '88015902',
+          neighborhood: `Bairro ${time}`,
+          city: `Cidade ${time}`,
+          uf: 'sc'
+        }
+      ],
+      contact: [
+        generateCellphone()
+      ]
+    }
+
+    const res = await utils.post(person)
+    expect(res.status).to.equal(HttpStatus.CREATED)
+
+    const insertedPerson = res.data
+    expect(insertedPerson._id).to.be.not.null
   })
 })
+
+const generateCellphone = () => {
+  let cellphone = '1'
+  const numbers = '023456789'
+
+  for (let index = 0;index < 10;index++) {
+    cellphone += numbers.charAt(Math.floor(Math.random() * numbers.length))
+  }
+
+  return cellphone
+}
