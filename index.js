03499f9eb6112c56e68775b8a5af05009503c526
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-07 09:47:27 -0300

    isolate server and database

--- index.js
+++ index.js
@@ -1,47 +1,7 @@
-const routes = require('./routes')
-const mongoose = require('mongoose')
-const swagger = require('./config/swagger')
-const config = require('./config/config')()
+const server = require('./server')
+const db = require('./db/db')
 
-const fastify = require('fastify')({
-  logger: true
-})
-
-fastify.register(require('fastify-swagger'), swagger.options)
-fastify.register(require('fastify-cors'), {
-  origin: config.url_client
-})
-
-routes.forEach((route) => {
-  fastify.route(route)
-})
-
-let mongo
-
-const start = async () => {
-  try {
-    mongo = await mongoose.connect(config.db)
-    console.log('MongoDB connected…')
-
-    await fastify.listen(config.port, '0.0.0.0')
-    fastify.swagger()
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
-
-const close = async () => {
-  await fastify.close()
-}
-
-const dropDatabase = async () => {
-  await mongoose.connection.dropDatabase()
-}
-
-module.exports = {
-  close,
-  dropDatabase
-}
+;(async () => {
+  await db.connect()
+  await server.initialize()
+})()
