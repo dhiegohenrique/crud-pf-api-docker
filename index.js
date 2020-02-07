84f8c5746475bc0d43f22ea34d3ef5ebda2ab5e1
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-07 15:44:17 -0300

    add test dependencies to dev dependencies

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
-// const server = require('./server')
-// const db = require('./db/db')
+const server = require('./server')
+const db = require('./db/db')
 
-// ;(async () => {
-//   await db.connect()
-//   await server.initialize()
-// })()
-
-
-const routes = require('./routes')
-const mongoose = require('mongoose')
-const swagger = require('./config/swagger')
-const config = require('./config/config')()
-
-const fastify = require('fastify')({
-  logger: true
-})
-
-fastify.register(require('fastify-swagger'), swagger.options)
-fastify.register(require('fastify-cors'), {
-  origin: config.url_client
-})
-
-routes.forEach((route) => {
-  fastify.route(route)
-})
-
-mongoose.connect(config.db, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-})
-  .then(() => console.log('MongoDB connected…'))
-  .catch(err => console.log(err))
-
-const start = async () => {
-  try {
-    await fastify.listen(config.port, '0.0.0.0')
-    fastify.swagger()
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
+;(async () => {
+  await db.connect()
+  await server.initialize()
+})()
